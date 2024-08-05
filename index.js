43117fb8258efef2a6aeff62ae8b904aa8b5963c
marimo123-sudo <galievr323@gmail.com>
2024-08-05 11:23:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,21 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e =
     document.body.classList.toggle('dark-mode', e.matches);
 });
 
+if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1		
+  var dpt = window.devicePixelRatio;
+  var widthM = window.screen.width * dpt;
+  var widthH = window.screen.height * dpt;
+  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
+}
 
+Telegram.WebApp.ready();
+Telegram.WebApp.setBackgroundColor('#000000'); // Устанавливаем черный цвет рамки
+
+document.addEventListener('gesturestart', function (e) {
+  e.preventDefault();
+});
+
+// Запрещает изменение масштаба через жесты на устройствах с сенсорным экраном
+document.addEventListener('touchmove', function (event) {
+  if (event.scale !== 1) { event.preventDefault(); }
+}, { passive: false });
